a8a183ab633aa49cf3e5f98ac15f676274850570
Julius Kirsch <dev@frozenassassine.de>
2025-04-09 09:27:20 +0200

    feat: correct dataset path

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ BATCHES = 64
 NOTE_SEQUENCE_LENGTH = 100
 
 #Data
-MIDIPATH = '/mnt/w/Documents/NN DATASETS/MidiSounds/2' # '/mnt/f/NN DATASETS/MidiSounds/midi_dataset/2'
+MIDIPATH = '/mnt/f/NN DATASETS/MidiSounds/midi_dataset/2'
 TRAINING_FILE_COUNT = 5
 
 #Out
